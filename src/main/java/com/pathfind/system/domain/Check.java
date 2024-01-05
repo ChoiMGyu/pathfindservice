0ed89e84268d61247a78e137f622a197cb2ab940
choi <nipper@naver.com>
2024-01-05 22:35:32 +0900

    no

--- src/main/java/com/pathfind/system/domain/Check.java
+++ src/main/java/com/pathfind/system/domain/Check.java
@@ -1,18 +1,15 @@
 /*
  * 클래스 기능 : 동의 여부 체크 엔티티
- * 최근 수정 일자 : 2024.01.05(금)
+ * 최근 수정 일자 : 2024.01.05
  */
 package com.pathfind.system.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "checks")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Check {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,17 +25,7 @@ public class Check {
     @Column(nullable = false, columnDefinition = "TINYINT(1)")
     private boolean dormant; //휴먼계정 여부확인
 
-    //==정적 팩토리 메서드==//
-    public static Check createCheck() {
-        Check check = new Check();
-        check.changeEmailAuth(false);
-        check.changeInformationAgree(false);
-        check.changeDormant(false);
-
-        return check;
-    }
-
-    //==비지니스 로직==//
+    //==setter 방지 메서드==//
     public void changeEmailAuth(boolean emailAuth) {
         this.emailAuth = emailAuth;
     }
@@ -49,5 +36,13 @@ public class Check {
 
     public void changeDormant(boolean dormant) {
         this.dormant = dormant;
+    }
+
+    public static Check createCheck() {
+        Check check = new Check();
+        check.changeEmailAuth(false);
+        check.changeInformationAgree(false);
+        check.changeDormant(false);
+        return check;
     }
 }
---
+++ src/test/java/com/pathfind/system/entity/EntityCreate.java
@@ -0,0 +1,46 @@
+package com.pathfind.system.entity;
+
+import com.pathfind.system.domain.Check;
+import com.pathfind.system.domain.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class EntityCreate {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("회원엔티티생성")
+    public void 회원엔티티생성() throws Exception
+    {
+        //given
+        String userId = "Id";
+        String password = "password";
+        String nickname = "nickname";
+        String email = "email";
+        Check check = Check.createCheck();
+
+        Member member = Member.createMember(userId, password, nickname, email, check);
+
+        em.persist(member);
+
+        //when
+        Member findMember = em.find(Member.class, member.getId());
+
+        //then
+        Assertions.assertThat(member.getUserId()).isEqualTo(findMember.getUserId());
+    }
+
+}
