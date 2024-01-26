0004b551996618daca7b37d89f8b6f8b76a69e7c
Seo b h <sbh00092@gmail.com>
2024-01-26 13:29:05 +0900

    로그인, 사인업 브랜치 통합

--- src/main/java/com/pathfind/system/controller/HomeController.java
+++ src/main/java/com/pathfind/system/controller/HomeController.java
@@ -9,6 +9,8 @@ import jakarta.mail.Session;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,20 +20,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class HomeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+
     @GetMapping("/")
     public String homeLogin(HttpServletRequest request, Model model) {
         HttpSession session = request.getSession(false);
-        if(session == null) {
+        if (session == null) {
             return "home";
         }
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
         session.setAttribute(SessionConst.SUBMIT_MEMBER, null);
 
         //세션에 회원 데이터가 없으면 home
-        if(loginMember == null) {
+        if (loginMember == null) {
             return "home";
         }
 
+/*        logger.info("loginMember.getCheck().isDormant: {}", loginMember.getId());
+        logger.info("loginMember.getCheck().isDormant: {}", loginMember.getCheck().isDormant());*/
         //휴면 계정이 홈화면으로 이동시에는 세션을 무효화하고 이동
         if(loginMember.getCheck().isDormant()) {
             session.invalidate();
