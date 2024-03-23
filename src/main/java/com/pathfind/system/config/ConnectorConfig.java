428ad88e2182b0b23dfb67aa6b7cb3c7e7cb6e14
ChoiMGyu <nippert@naver.com>
2024-03-23 14:26:35 +0900

    리다이렉트 없이 http, https 모두 사용

---
+++ src/main/java/com/pathfind/system/config/ConnectorConfig.java
@@ -0,0 +1,35 @@
+package com.pathfind.system.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConnectorConfig {
+
+    @Value("${server.http.port}")
+    private int serverPortHttp;
+
+    @Value("${server.port}")
+    private int serverPortHttps;
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(serverPortHttp);
+        return connector;
+    }
+}
