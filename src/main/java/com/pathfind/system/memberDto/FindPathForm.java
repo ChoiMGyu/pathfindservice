00601ed11ec1351ff24901f1d0106edcaad7428a
Seo b h <sbh00092@gmail.com>
2024-04-06 23:40:56 +0900

    방 만들기 버튼 구현, 자잘한 오류 수정.

---
+++ src/main/java/com/pathfind/system/memberDto/FindPathForm.java
@@ -0,0 +1,10 @@
+package com.pathfind.system.dto;
+
+import lombok.Data;
+
+@Data
+public class FindPathForm {
+    public String startObject;
+    public String endObject;
+    public String transportation;
+}
