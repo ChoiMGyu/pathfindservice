b84293867be3a390859aaa007a5e7bf10b70b1b3
Seo b h <sbh00092@gmail.com>
2024-07-19 11:51:09 +0900

    회원가입 API 구현 시작

--- src/main/java/com/pathfind/system/exception/ErrorCode.java
+++ src/main/java/com/pathfind/system/exception/ErrorCode.java
@@ -9,11 +9,16 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum ErrorCode {
+public enum ErrorCode implements BasicErrorCode {
     INVALID_INPUT_VALUE(400, "-400", "올바른 값을 입력하지 않은 경우"),
     ROOM_EXCEEDED(403, "-403", "방 최대 인원인 5명보다 많은 사람을 초대하는 경우");
 
     private final int status;
     private final String code;
     private final String description;
+
+    @Override
+    public ErrorReason getErrorReason() {
+        return ErrorReason.builder().description(description).code(code).status(status).build();
+    }
 }
---
+++ src/main/java/com/pathfind/system/exception/ErrorReason.java
@@ -0,0 +1,12 @@
+package com.pathfind.system.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ErrorReason {
+    private final Integer status;
+    private final String code;
+    private final String description;
+}
